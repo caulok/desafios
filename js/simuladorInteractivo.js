af86bf2d869ef1c1f6b011ea5b2273e1075a235c
caulok <ramicaulo@gmail.com>
2022-06-24 01:35:03 -0300

    Sumando objetos y condicionales.

--- js/simuladorInteractivo.js
+++ js/simuladorInteractivo.js
@@ -1,47 +1,87 @@
-let valorPasaje = 10000 
-let porc = 0.9
-let nombre = prompt(`Hola, ¿podrías indicarnos cuál es tu nombre?`)
-alert(`Tu valor de pasaje para el año 2022 es de:\nARS $${valorPasaje}\n\nProgramando a partir del 2024 podrás obtener descuentos.`)
-
-function fecha(dd,mm,yy){
-    let date = dd + "/" + mm + "/" + yy
-    alert(`Tu viaje será el ${date}.`)
-}
-let dia = parseInt(prompt(`Ingresa el día de viaje.`))
-let mes = parseInt(prompt(`Ingresa el mes de viaje.`))
-let anio = parseInt(prompt(`Ingresa el año de viaje.`))
-
-if (dia === 0 || dia >= 32) {
-    alert(`Ingresa un día válido`)
-    dia = parseInt(prompt(`Ingresa nuevamente el día de viaje.`))
-}
-else if (mes === 0 || mes >= 13 ){
-    alert(`Ingresa un mes válido`)
-    mes = parseInt(prompt(`Ingresa nuevamente el mes de viaje.`))
-}
-else if(anio === 0 || anio < 2022){
-    alert(`Ingresa un mes válido`)
-    anio = parseInt(prompt(`Ingresa nuevamente el año de viaje.`))
+let usuario = prompt(`Hola, ¿podrías indicarnos cuál es tu nombre?`)
+while(usuario == 0){
+    alert(`No has ingresado tu nombre`)
+    usuario = prompt(`Por favor informanos tu nombre`)
 }
 
-function descuento(valor1, valor2){
-    let discount = valor1 * valor2
-    let total = discount*cantidad 
-    if(anio >= 2024){
-        alert(`Aplicando un descuento del 10% por tiempo de programación, el valor total por ${cantidad} personas es de:\n\nARS $${total}.`)
+class Paquete {
+    constructor(nombre, mes, anio, aerolinea, comida, precioVenta){
+        this.nombre = nombre.toUpperCase()
+        this.mes = mes
+        this.anio = anio
+        this.aerolinea = aerolinea
+        this.comida = comida
+        this.precioVenta = precioVenta
     }
 }
 
-let cantidad = parseInt(prompt(`Ingresa por favor la cantidad de personas que viajarán con vos`))
-
-for (let index = 1; index <= cantidad; index++) {
-    let nombres = prompt(`Ingresa por favor el nombre de la persona ${index}`)
-
-    if(nombres == 0){
-        alert(`No ingresaste ningún nombre.`)
+function agregarPaquetes(){
+    let personas = parseInt(prompt(`¿Cuántas personas viajarán a Madrid?`))
+    while(personas == 0){
+        alert(`No has ingresado un número de personas`)
+        personas = prompt(`Por favor ingresa un número`)
     }
+
+    let paquete = []
+
+    for(i=1; i<=personas; i++){
+        let nombre = prompt(`¿Cual es el nombre de la persona ${i}?`)
+
+        let mes = parseInt(prompt(`Ingrese el mes de vuelo`))
+        while(mes == 0 || mes >= 13){
+            alert(`No has ingresado un mes válido`)
+            mes = prompt(`Por favor informanos un mes válido`)
+        }
+        let anio = parseInt(prompt(`Ingrese el año de vuelo`))
+        while(anio == 0 || anio < 2022){
+            alert(`No has ingresado un año válido`)
+            anio = prompt(`Por favor informanos un año válido`)
+        }
+
+        let aerolinea = parseInt(prompt(`Ingresa la aerolinea con la que viajas:\n\n1.Aerolineas Argentinas\n2.LATAM\n3.Iberia\n4.KLM\n5.Qatar Airways`))
+        if (aerolinea < 3) {
+            precioVenta = 500
+        } else if (aerolinea === 3) {
+            precioVenta = 750
+        } else {
+            precioVenta = 1200
+        }
+        while(aerolinea == 0){
+            alert(`No has ingresado la aerolinea`)
+            aerolinea = prompt(`Por favor informanos el número de aerolinea`)
+        }
+        
+        let comida = parseInt(prompt(`¿Sigues alguna dieta en particular?\n\n1.No\n2.Celíaco\n3.Kosher\n4.Vegano/Vegetariano`))
+        switch(comida) {
+            case 1: 
+                alert(`Seleccionaste que no tienes que seguir una dieta`)
+                break
+            case 2: 
+                alert(`Seleccionaste que necesitas comida celíaca`)
+                break
+            case 3: 
+                alert(`Seleccionaste que necesitas comida kosher`)
+                break
+            case 4: 
+                alert(`Seleccionaste que necesitas comida vegana/vegetariana`)
+                break
+            default:
+                alert(`Se seleccionó por defecto que no tienes que seguir ninguna dieta`)
+                break
+        }
+
+        let nuevoPaquete = new Paquete(nombre, mes, anio, aerolinea, comida, precioVenta)
+        paquete.push(nuevoPaquete)
+    }
+    console.log(paquete)
+    
+    // Esta parte de abajo sinceramente la busqué en internet ya que desconocía cómo sumar elementos de un array en un ciclo FOR.
+
+    const total = paquete.map(item => item.precioVenta).reduce((prev, curr) => prev + curr, 0);
+
+    alert(`El valor total del paquete para ${personas} personas a Madrid es de US$${total}`);
+
+    alert(`¡Gracias por tu compra ${usuario}! Nos vemos pronto`)
 }
 
-fecha(dia, mes, anio)
-descuento(valorPasaje, porc)
-alert(`¡Gracias por elegirnos y viajar con nosotros, ${nombre}!`)
+agregarPaquetes()
