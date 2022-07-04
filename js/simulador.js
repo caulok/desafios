31d9dc846297a0231be500b3d85b94ca3b8713ff
caulok <ramicaulo@gmail.com>
2022-07-03 23:25:08 -0300

    Simulador de reserva de paquetes

--- js/simulador.js
+++ js/simulador.js
@@ -2,17 +2,15 @@
 
     Una reserva que está compuesta de 1 paquete y personas. Los usuarios pueden elegir cualquiera de los 3 paquetes (Madrid, Toledo y Aranjuez). Cada uno definido en una fecha determinada y una cantidad de días establecidos. Luego el objetivo es mostrar un resumen de la reserva.*/
 
-/*1ro las clases*/
+/* CLASES */
 
 class Reserva {
-    constructor(id, generacion, paquete, persona){
-        this.id = id;
+    constructor(generacion, paquete, persona){
         this.generacion_de_ticket = generacion;
         this.paquete = paquete;
         this.persona = persona;
     }
 }
-
 class Paquete {
     constructor(id, ubicacion, fecha, dias, hospedaje, precio){
         this.id = id;
@@ -23,11 +21,10 @@ class Paquete {
         this.precio = precio;
     }
 }
-
 class Persona {
     constructor(name, pasaporte) {
-        this.nombre = prompt(name);
-        this.pasaporte = parseInt(pasaporte);
+        this.nombre = name;
+        this.pasaporte = pasaporte;
     }
 }
 
@@ -35,8 +32,6 @@ class Persona {
 let personas = [];
 let paquetes = [];
 let reservas = [];
-
-// let persona = new Persona() ----- ACÁ SE COMPLICA, NO ESTÁ CORRECTO,NO?
 
 let paqueteMadrid = new Paquete(1, "Madrid", new Date("2024-11-24"), 7, "Hilton Hotel", 13000);
 let paqueteToledo = new Paquete(2, "Toledo", new Date("2024-3-14"), 10, "Nuestro Camino Hotel", 6000);
@@ -55,22 +50,50 @@ const validarIsNotString = (dato) => {
     return true;
 }
 
-/* Ingreso de los datos */
+/* INGRESO DE LOS DATOS */
+    
+    /* Ingreso usuario */
 let usuario = prompt(`Hola, ¿podrías indicarnos cuál es tu nombre?`);
-while(usuario === "" || validarIsNotString(usuario)){
-    alert(`No has ingresado tu nombre`);
+while(usuario === "" || validarIsNotString(usuario) || usuario.length < 2){
+    alert(`No has ingresado tu nombre o es demasiado corto`);
     usuario = prompt(`Por favor informanos tu nombre`);
 }
-let elegirPaquete = parseInt(prompt(`Elegí por favor un paquete:\n\n1.Madrid\n2.Toledo\n3.Aranjuez`))
+    /* Elección de paquete */
+let elegirPaquete = parseInt(prompt(`Elegí por favor un paquete:\n\n1.Madrid\n2.Toledo\n3.Aranjuez`));
 paqueteElegido = paquetes.find(paquete => paquete.id == elegirPaquete);
 
-let reserva = new Reserva(1, Date(), paqueteElegido, Persona)
+    /* Elección de personas */
+let cantidad = parseInt(prompt(`¿Cuántas personas viajarán?`));
 
-reservas.push(reserva)
+function cantidadPersonas(){
+    for(i=1;i<=cantidad;i++){
+        let names = prompt(`Ingresa el nombre de la persona ${i}`);
+        while(names === "" || validarIsNotString(names) || usuario.length < 2){
+            alert(`No has ingresado un nombre o es demasiado corto`);
+            names = prompt(`Por favor informanos el nombre de la persona ${i}`);
+        }
+        let passports = parseInt(prompt(`Ingresa tu pasaporte`));
+                while(passports === "" || passports.toString().length < 6){
+                    alert(`No has ingresado tu pasaporte o colocaste un número menor a 6 dígitos`);
+                    passports = prompt(`Por favor informanos tu pasaporte`);
+        }
+    }
+    return{names,passports};
+}
+
+let names = cantidadPersonas.names;
+let passports = cantidadPersonas.passports;
+
+let people = new Persona(names, passports);
+personas.push(people);
+
+    /* RESERVA */
+let reserva = new Reserva(Date(), paqueteElegido, people);
+reservas.push(reserva);
 
 
-/*3ro laejecución de funciones*/
+/*3ro la ejecución de funciones*/
+cantidadPersonas();
 console.log(reserva);
-console.log(reserva.paquete);
-alert(`En la consola están los resultados de tu reserva ${usuario}`)
+alert(`En la consola están los resultados de tu reserva. Sugerimos que te fijes allí ${usuario}.\n\nTIP: Podés desplegar la reserva para ver el paquete y tus datos.`)
 
